09b020fac21e7075c4c4b3562bcfc1301afb5f12
ramazan kanbur <ramazanknbr@gmail.com>
2020-04-23 21:45:12 +0300

    salting and hashing

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -3,6 +3,7 @@ require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const requireAuth = require('./middlewares/requireAuth');
 
 const authRouter = require('./routes/authRoutes');
 
@@ -11,25 +12,25 @@ const app = express();
 app.use(bodyParser.json());
 app.use(authRouter);
 
-const mongoURI = 'mongodb+srv://trackClusterAdmin:trackClusterAdmin@trackcluster-8oy8n.mongodb.net/test?retryWrites=true&w=majority';
+const mongoURI =
+  'mongodb+srv://trackClusterAdmin:trackClusterAdmin@trackcluster-8oy8n.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useCreateIndex: true
+  useNewUrlParser: true,
+  useCreateIndex: true,
 });
 
 mongoose.connection.on('connected', () => {
-    console.log('connected to mongo instance');
-
+  console.log('connected to mongo instance');
 });
 
 mongoose.connection.on('error', (err) => {
-    console.error('Error connecting to mongo', err);
-})
+  console.error('Error connecting to mongo', err);
+});
 
-app.get('/', (req, res) => {
-    res.send("hello");
+app.get('/', requireAuth, (req, res) => {
+  res.send(`your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
-    console.log('Listenin on port 3000');
+  console.log('Listenin on port 3000');
 });
